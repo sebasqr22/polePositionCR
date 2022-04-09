abd35144c4d5ba0b5a1f6a81d631da3b4ad24d59
Mario <mjcarranza@users.noreply.github.com>
2022-04-09 15:09:10 -0600

    Atualizacion del juego

--- Cliente-Servidor-Java-C/Juego/main.c
+++ Cliente-Servidor-Java-C/Juego/main.c
@@ -4,6 +4,8 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
 int main()
 {
@@ -11,6 +13,9 @@ int main()
     al_init();
     al_init_primitives_addon();
     al_init_image_addon();
+    al_init_font_addon();
+    al_init_ttf_addon();
+    ALLEGRO_FONT *font = al_load_font("hoffmann.TTF",20,0);
     // Se crea una ventana nueva
     ALLEGRO_DISPLAY *ventana = 0;
     ventana = al_create_display(1000, 600);
@@ -45,56 +50,76 @@ int main()
     al_register_event_source(queue, al_get_timer_event_source(timer));
     
     // curvatura, distancia de la curvatura
-    float road[11][2] = {{0.0f,10.0f},{0.0f,200.0f},{1.0f,200.0f},{0.0f,400.0f},{-1.0f,100.0f},
-                            {0.0f,200.0f},{-1.0f,200.0f},{1.0f,200.0f},{0.0f,200.0f},{0.2f,500.0f},{0.0f,200.0f}};
+    float road[11][2] = {{0.0f ,10.0f},
+                         {0.0f ,200.0f},
+                         {1.0f ,600.0f},
+                         {0.0f ,400.0f},
+                         {-1.0f,600.0f},
+                         {0.0f ,200.0f},
+                         {-1.0f,600.0f},
+                         {1.0f ,600.0f},
+                         {0.0f ,200.0f},
+                         {0.2f ,500.0f},
+                         {0.0f ,200.0f}};
     // Variables para el control del juego
     float carSpeed = 0.0f;
     float curvatura = 0.0f;
-    float x = 0;
+    float roadCurv = 0.0f;
+    float jugadorCurv = 0.0f;
+    float carPos = 0;
+    float carPosition = 0;
     float distancia = 0.0f;
     float dist = 0.0f;
-    float ellapsed = 0.0f;
-    
+    long time1 = clock();
+    long time2, eTime;
+    double ellapsed_time;
 
     // loop del juego // crear conexión con servidor antes de esto
     while(!done){
+        // Manejo del tiempo
+        time2 = clock();
+        eTime = time2- time1;
+        time1 = time2;
+        ellapsed_time = (double)eTime/CLOCKS_PER_SEC;
+        //printf("El ellkapsed tim es: %lf",ellapsed_time);
+        
+
+
         // Variables para el control de los eventos del juego
         ALLEGRO_KEYBOARD_STATE keyState;
         ALLEGRO_EVENT evt;
         al_get_keyboard_state(&keyState);
         al_wait_for_event(queue, &evt);
 
-        int roadSection = 0;
-        float offset = 0;
-
         // Si se cierra la ventana creada, finaliza el juego
         if (al_key_down(&keyState, ALLEGRO_KEY_ESCAPE) || evt.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
         {
             done = true;
         }
+
+        int roadSection = 0;
+        float offset = 0;
         // Se encuentra la posicion del jugador en la pista
+        //         1       <   11            10.0    <=    d
         while (roadSection < sizeof(road) && offset <= distancia){
-            offset += road[roadSection][1];
-            roadSection++;
+            offset += road[roadSection][1]; // 10.0
+            roadSection++; // 10
         }
+        //printf("El ellkapsed tim es: %d\n", roadSection);
 
-        if (roadSection==2 || roadSection==7 || roadSection==9)
-            ellapsed+=0.02f;
-
-        if (roadSection==4 || roadSection==6)
-            ellapsed+=-0.02f;
-        else
-            ellapsed=0.3f;
 
         float targetCurvature = road[roadSection-1][0];
-        float roadCurveDiff = (targetCurvature - curvatura)*ellapsed; // elapsed
-        curvatura = roadCurveDiff;
+        float roadCurveDiff = (targetCurvature - curvatura)*(ellapsed_time + 0.05) * carSpeed; // elapsed
+        curvatura = roadCurveDiff; 
+
+        printf("Curva: %f\n", carPos);
+
+        roadCurv += (curvatura)* ellapsed_time * carSpeed;
 
         al_flip_display();
         al_clear_to_color(al_map_rgb(79, 79, 79));
         al_draw_bitmap(horizonte,0,0,0); // img, X, Y, flags
-        al_draw_bitmap(CarFront, x + 462,500,0);
-        //al_draw_filled_rectangle(0, 200, 10, 210,al_map_rgb(0,255,0));
+        
 
         int screenWidth = 1000;
         int screenHeight = 600;
@@ -103,7 +128,7 @@ int main()
             for (int xpos = 0; xpos < screenWidth; xpos++)
             {
                 float perspectiva = (float)ypos / 400;
-                float fPuntoMedio = 0.5f + curvatura * pow((1.0f - perspectiva), 3);
+                float fPuntoMedio = 0.5f + curvatura * pow((1.0f - perspectiva),3);
                 float fAnchoPista = 0.1f + perspectiva * 0.8f;
                 float fAnchoClip = fAnchoPista * 0.15f;
 
@@ -116,6 +141,7 @@ int main()
                 int nRow = 200 + ypos;
                 int verde;
                 int r,g,b;
+
                 // Se evalua la funcion seno para crear la perspectiva.
                 if(sinf(20.0f *powf(1.0f - perspectiva, 3) + distancia*0.1f) > 0){
                     verde = 255;
@@ -147,37 +173,56 @@ int main()
                     al_draw_filled_rectangle(xpos, 200 + ypos, 10+xpos, 210 + ypos,al_map_rgb(0,verde,0));
                 xpos += 5;
             }
-            //ypos += 1;
         }
+        
+        al_draw_bitmap(CarFront, carPos + 462,500,0);
 
         // Si se presiona la tecla direccional DERECHA, el carro se mueve a la derecha con una pequeña animación
         if (al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
         {
-            al_draw_bitmap(CarDer, x + 462,500,0);
-            x += 20;
+            al_draw_bitmap(CarDer, carPos + 462,500,0);
+            carSpeed += 2.0f * (ellapsed_time + 0.001);
+            jugadorCurv += 0.7f * ellapsed_time;
+            carPos += 20;
         }
-        // Si se presiona la tecla ARRIBA, 
-        if (al_key_down(&keyState, ALLEGRO_KEY_UP))
-        {
-            distancia += 5.0f * 0.6f;
-            dist += 30.0f * 0.6f;
-        }
+
         // Si se presiona la tecla direccional IZQUIERDA, el carro se mueve a la izquierda con una pequeña animación
         if (al_key_down(&keyState, ALLEGRO_KEY_LEFT))
         {
-            al_draw_bitmap(CarIzq, x + 462,500,0);
-            x -= 20;
+            al_draw_bitmap(CarIzq, carPos + 462,500,0);
+            carSpeed += 2.0f * (ellapsed_time + 0.001);
+            jugadorCurv -= 0.7f * ellapsed_time;
+            carPos -= 20;
         }
-        
-        
-        
-        /*
-        if (x > width)
-        {
-            x = -al_get_bitmap_width(horizonte);
+
+        // Si se presiona la tecla ARRIBA, se acelera 
+        if (al_key_down(&keyState, ALLEGRO_KEY_UP)){
+            carSpeed += 2.0f * (ellapsed_time + 0.001);
+            if(targetCurvature > 0)
+                carPos -= 10;
+            if(targetCurvature < 0)
+                carPos += 10;
+
         }
-        */
+        else
+            carSpeed -= 1.0f * ellapsed_time;
+
+        // Se limita la velocidad del carro
+        if(carSpeed<0.0f) carSpeed = 0.0f;
+        if(carSpeed>1.0f) carSpeed = 1.0f; 
+
+        // Se mueve el carro hacia los lados de acuerdo con su velocidad
+        distancia += (70.0f * carSpeed) * ellapsed_time;
+        dist += (70.0f * carSpeed)*ellapsed_time;
+
         
+        if(carPos <= -380.0f || carPos >= 380.0f) // if(fabs(jugadorCurv - roadCurv) >= 0.8f)
+           carSpeed -= 5.0f *ellapsed_time;
+        
+        al_draw_text(font,al_map_rgb(0,0,0),5,5,ALLEGRO_ALIGN_LEFT,"VIDA:");
+        al_draw_text(font,al_map_rgb(0,0,0),5,20,ALLEGRO_ALIGN_LEFT,"DISTANCIA:");
+        al_draw_text(font,al_map_rgb(0,0,0),5,35,ALLEGRO_ALIGN_LEFT,"PUNTOS:");
+        al_draw_text(font,al_map_rgb(0,0,0),5,50,ALLEGRO_ALIGN_LEFT,"VELOCIDAD:");
     }
     
 
@@ -187,6 +232,7 @@ int main()
     al_destroy_bitmap(CarFront);
     al_destroy_bitmap(CarDer);
     al_destroy_bitmap(CarIzq);
+    al_destroy_font(font);
     al_destroy_display(ventana);
     al_destroy_timer(timer);
     al_uninstall_keyboard();
