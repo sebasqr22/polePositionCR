2aca9edf1df1aa2ce7d679d7cc3626010a551340
Mario <48070830+mjcarranza@users.noreply.github.com>
2022-04-11 16:06:27 -0600

    Update main.c

--- Cliente-Servidor-Java-C/Juego/main.c
+++ Cliente-Servidor-Java-C/Juego/main.c
@@ -93,7 +93,7 @@ bool hayColision(float xCar, float yCar, float xObject, float yObject, float rCa
 
 // Se calcula el radio de de la elipse
 float radio(float a, float b,float cb, float h){
-    float r = b/(sqrtf(1-(a-(b/a)) * (powf(cosf(asinf(cb/h)), 2))));
+    float r = b/(sqrtf(1-(a-(powf(b,2)/a)) * (powf(cosf(asinf(cb/h)), 2))));
     return r;
 }
 
